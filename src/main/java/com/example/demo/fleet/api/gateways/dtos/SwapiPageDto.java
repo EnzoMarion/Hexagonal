be3bf11e9c592667448732e2bc756b1a9e3adcc2
EnzoMarion <marionenzo26@gmail.com>
2025-11-20 11:58:33 +0100

    fix spring

--- src/main/java/com/example/demo/fleet/api/gateways/dtos/SwapiPageDto.java
+++ src/main/java/com/example/demo/fleet/api/gateways/dtos/SwapiPageDto.java
@@ -1,15 +1,13 @@
 package com.example.demo.fleet.api.gateways.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
 public class SwapiPageDto {
     private List<SwapiStarshipDto> results;
 
-    public List<SwapiStarshipDto> getResults() {
-        return results;
-    }
-
-    public void setResults(List<SwapiStarshipDto> results) {
-        this.results = results;
-    }
 }
